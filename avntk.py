10f945a013c3bc60f9dbc883f3be1d099372af90
Daniel Benalcazar <danielbenalcazar87@gmail.com>
2022-05-22 15:50:01 -0500

    Show frames on test

--- avntk.py
+++ avntk.py
@@ -9,6 +9,12 @@ from model_architecture import build_tools
 from utils import data_tools
 from config import *
 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+session = tf.Session(config=config)
+print('\n'*2 + '--Memory Growth enabled--' + '\n'*2)
+
 tf.reset_default_graph()
 tf.set_random_seed(0)
 random.seed(0)
@@ -34,8 +40,8 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, verbo
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_save_folder, histogram_freq=0, write_graph=True,
                                                       write_images=False)
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(os.path.join(base_folder,'files','inference_video.avi'),fourcc, 12.0, (width*4,height*4))
+#fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+#out = cv2.VideoWriter(os.path.join(base_folder,'files','inference_video.mp4'),fourcc, 12.0, (width*4,height*4))
 
 
 def _trainer(network,train_generator,val_generator):
@@ -45,30 +51,34 @@ def _trainer(network,train_generator,val_generator):
     with open(os.path.join(base_folder,'files',model_name,'training_logs.json'),'w') as w:
         json.dump(history.history,w)
 
-def inference(network,video_file):
-    print("hgcahjbc")
+def inference(network, video_file):
+    cv2.namedWindow("CNN LSTMN Inference", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("CNN LSTMN Inference", 840,560) # 1440,810
+    print("\nProcessing Frames ...")
     image_seq = deque([],8)
     cap = cv2.VideoCapture(video_file)
-    counter = 0 
+    counter = 0
     stat = 'safe'
     while (cap.isOpened()):
         ret, frame = cap.read()
         if ret:
             _frame = cv2.resize(frame,(width,height))
             image_seq.append(_frame)
-            if counter%2 == 0:
-                if len(image_seq)==8:
-                    np_image_seqs = np.reshape(np.array(image_seq)/255,(1,time,height,width,color_channels))
-                    r = network.predict(np_image_seqs)
-                    stat = ['safe', 'collision'][np.argmax(r,1)[0]]
-            
+            if len(image_seq)==8:
+                np_image_seqs = np.reshape(np.array(image_seq)/255,(1,time,height,width,color_channels))
+                r = network.predict(np_image_seqs)
+                stat = ['safe','collision'][np.argmax(r,1)[0]]
+
             cv2.putText(frame,stat, (230,230), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,255,0),3)
-            out.write(frame)
+            #out.write(frame)
+            cv2.imshow("CNN LSTMN Inference",frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
             counter+=1
             print (counter)
         else:
             cap.release()
-            out.release()
+            #out.release()
             cv2.destroyAllWindows()
 
 
@@ -76,7 +86,7 @@ if  __name__ == "__main__":
 
     model_tools = build_tools()
     network = model_tools.create_network(model_name)
-
+    
     if mode == 'train':
         train_generator = data_tools(train_folder,'train')
         valid_generator = data_tools(valid_folder,'valid')
@@ -84,10 +94,15 @@ if  __name__ == "__main__":
 
     else:
         network.load_weights(os.path.join(model_save_folder,'model_weights_032.ckpt'))
-        inference(network,os.path.join(base_folder,'files','out1.avi'))
+        
+        inference(network, os.path.join(base_folder,'files/samples', input_video))
 
         #testing from batch
-        '''test_generator = get_valid_data(test_folder)
+        '''
+        test_generator = get_valid_data(test_folder)
         for img_seq,labels in test_generator:
             r = network.predict(img_seq)
-            print ('accuracy',np.count_nonzero(np.argmax(r,1)==np.argmax(labels,1))/8)'''
+            print ('accuracy',np.count_nonzero(np.argmax(r,1)==np.argmax(labels,1))/8)
+        '''
+
+    print('\n\nSuccessful {}!!! XD\n'.format(mode))
