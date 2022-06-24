10872f53e022e9594fc9d1ecbd8d82fb078bf56f
Daniel Benalcazar <danielbenalcazar87@gmail.com>
2022-06-24 16:34:24 -0500

    First trainings

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-epochs = 52
+epochs = 400
 time = 8
 n_classes = 2
 width, height, color_channels = (210, 140, 3)
@@ -8,12 +8,12 @@ number_of_hiddenunits = 32
 batch_size = 16
 save_freq = 1
 
-data_type = 'cars' # iris_2classes # iris_4classes #
+data_type = 'iris_2classes' # 'cars' # 'iris_4classes' #
 model_name = 'vgg' # 'inception' #
 version = 'v1'
 
-mode = 'test' # 'train' # 
-test_epoch = 32
+mode = 'test' # 'train' #
+test_epoch = 387
 
 input_video = 'GTA_V_3.mp4' # 'GTA_V_1.mp4' # 'GTA_V_2.mp4' #
 
